55f2b4d5a542a7e1fc13444a50a1112da4c8264d
MarkStiff <LZH1314521ligao@163.com>
2022-10-02 12:16:48 +0800

    Site updated: 2022-10-02 12:16:48

---
+++ js/weather.js
@@ -0,0 +1,12 @@
+(function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+  window.SeniverseWeatherWidget('show', {
+    flavor: "slim",
+    location: "WQJ6YY8MHZP0",
+    geolocation: true,
+    language: "zh-Hans",
+    unit: "c",
+    theme: "auto",
+    token: "07160b96-d6a5-4d6e-9797-aed907756172",
+    hover: "enabled",
+    container: "tp-weather-widget"
+  })
